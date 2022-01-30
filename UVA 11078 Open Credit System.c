175abbdbb1fee8e43c63fec6005956dc69a07827
EZAZ-2281 <81481142+EZAZ-2281@users.noreply.github.com>
2022-01-30 16:19:48 +0600

    UVA

---
+++ UVA 11078 Open Credit System.c
@@ -0,0 +1,31 @@
+/* Bismillahir Rahmanir Raheem */
+
+#include<stdio.h>
+int main()
+{
+    int T, t; 
+    scanf("%d", &T); 
+    for(t = 0; t < T; t++)
+    {
+        int m, i; 
+        scanf("%d", &m); 
+        int a[m]; 
+        for(i = 0; i < m; i++){
+            scanf("%d", &a[i]); 
+        }
+        int max = -5; 
+        int difference = -5;  
+        for(i = 0; i < m-1; i++)
+        {
+            if(a[i] > max){
+                max = a[i]; 
+            }
+            if(max - a[i+1] > difference){
+                difference = max - a[i+1]; 
+            }
+        }
+        printf("%d\n", difference); 
+        /* printf("%d\n", max); */
+    }
+    return 0; 
+}
