0ec9db25f00ece2a977bb5a12f933b8619a0f7e7
EZAZ-2281 <81481142+EZAZ-2281@users.noreply.github.com>
2022-01-31 13:37:35 +0600

    CF

---
+++ CF 1180A Alex and a Rhombus.c
@@ -0,0 +1,11 @@
+/* Bismillahir Rahmanir Raheem */
+
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int value = n * n + (n-1) * (n-1);
+    printf("%d\n", value);
+    return 0;
+}
