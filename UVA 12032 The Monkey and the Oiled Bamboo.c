22bd77d42e5f36151b24d9180499f71221374a36
Golam Kibria Ezaz <golamkibria3435@gmail.com>
2022-03-13 11:43:02 +0600

    UVA

--- UVA 12032 The Monkey and the Oiled Bamboo.c
+++ UVA 12032 The Monkey and the Oiled Bamboo.c
@@ -31,3 +31,35 @@ int main()
     }
     return 0;
 }
+
+/*
+#include <stdio.h>
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    int j;
+    for(j = 0; j < t; j++)
+    {
+        int n;
+        scanf("%d", &n);
+        int a[100005], i;
+        for(i = 0; i < n; i++)
+        {
+            scanf("%d", &a[i]);
+        }
+        int k = a[1] - a[0];
+        int sol = k;
+        for(i = 0; i < n-1; i++)
+        {
+            if(a[i+1] - a[i] == k)
+                k--;
+            else if(a[i+1] - a[i] > k)
+                break;
+        }
+        printf("Case %d: %d\n", j+1, sol);
+    }
+
+    return 0;
+}
+*/
