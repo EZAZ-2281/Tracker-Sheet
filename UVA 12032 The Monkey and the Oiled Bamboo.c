71d516b6471bb413a42227647f42aaa04f1f3422
Golam Kibria Ezaz <golamkibria3435@gmail.com>
2022-03-19 23:43:19 +0600

    UVA

--- UVA 12032 The Monkey and the Oiled Bamboo.c
+++ UVA 12032 The Monkey and the Oiled Bamboo.c
@@ -1,65 +1,31 @@
-/* Bismillahir Rahmanir Raheem */
-
 #include <stdio.h>
-
 int main()
 {
-    int t, n, p;
+    int t, x;
     scanf("%d", &t);
-    for(p = 0; p < t; p++)
+    for(x = 1; x <= t; x++)
     {
+        int n, k = 0, i;
         scanf("%d", &n);
-        int a[100001], i;
-        for(i = 0; i < n; i++){
+        int a[100001];
+        for(i = 1; i <= n; i++){
             scanf("%d", &a[i]);
         }
-        int key = 0, ans;
-        for(i = 0; i < n; i++) {
-            if(a[i+1]-a[i] > key)
-                key = a[i+1]-a[i];
+        for(i = 1; i <= n; i++){
+            if(a[i] - a[i-1] > k){
+                k = a[i] - a[i-1];
+            }
         }
-        ans = key;
-        for(i = 0; i < n; i++) {
-            if(a[i+1]-a[i] == key)
-                key--;
-            else if(a[i+1]-a[i] > key) {
-                ans++;
+        int count = k;
+        for(i = 1; i <= n; i++){
+            if(a[i] - a[i-1] == k)
+                k--;
+            else if(a[i] - a[i-1] > k){
+                count++;
                 break;
             }
         }
-        printf("Case %d: %d\n", p+1, ans);
+        printf("Case %d: %d\n", x, count);
     }
     return 0;
 }
-
-/*
-#include <stdio.h>
-int main()
-{
-    int t;
-    scanf("%d", &t);
-    int j;
-    for(j = 0; j < t; j++)
-    {
-        int n;
-        scanf("%d", &n);
-        int a[100005], i;
-        for(i = 0; i < n; i++)
-        {
-            scanf("%d", &a[i]);
-        }
-        int k = a[1] - a[0];
-        int sol = k;
-        for(i = 0; i < n-1; i++)
-        {
-            if(a[i+1] - a[i] == k)
-                k--;
-            else if(a[i+1] - a[i] > k)
-                break;
-        }
-        printf("Case %d: %d\n", j+1, sol);
-    }
-
-    return 0;
-}
-*/
